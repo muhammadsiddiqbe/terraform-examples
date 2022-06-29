63bd7ef98fb9b00af02822e85881e5f77fe6390d
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-06-29 13:20:15 +0500

    modify

--- main.tf
+++ main.tf
@@ -10,19 +10,17 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features {
-    
+
   }
   subscription_id = "f5e76248-4a36-46c4-a53a-399f94f9e210"
 }
 
 resource "azurerm_resource_group" "aks-rg2" {
-  name     = "aks-rg2-tf"
+  name     = "my-terraform-gr"
   location = "Central US"
 
-# Add Tags
-#  tags = {
-#    "environ ment" = "k8sdev"
-#    "demotag"     = "refreshtest"
-#  }
+ tags = {
+   "environment" = "k8sdev"
+ }
 
 }
