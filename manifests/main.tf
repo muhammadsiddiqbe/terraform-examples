8f3c5008612c61053f38de4bcc876c8e9f8c7842
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-06-30 11:59:00 +0500

    add these files

--- manifests/main.tf
+++ manifests/main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.0.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -22,4 +27,11 @@ resource "azurerm_resource_group" "aks-rg2" {
   tags = {
     "environment" = "k8sdev"
   }
+}
+
+resource "azurerm_log_analytics_workspace" "insights" {
+  name                = "logs-${random_pet.aksrandom.id}"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  retention_in_days   = 30
 }
