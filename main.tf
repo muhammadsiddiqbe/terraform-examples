6856d2aa67a9b2a8e87ac9f3d227242fe3f06279
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-07-01 12:12:10 +0500

    update

--- main.tf
+++ main.tf
@@ -28,10 +28,3 @@ resource "azurerm_resource_group" "aks-rg2" {
     "environment" = "k8sdev"
   }
 }
-
-resource "azurerm_log_analytics_workspace" "insights" {
-  name                = "logs-${random_pet.aksrandom.id}"
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  retention_in_days   = 30
-}
