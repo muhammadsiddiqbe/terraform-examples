8d5ec627a30ce84da8671c86669f0834a888bf7f
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-07-05 13:04:39 +0500

    update

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ provider "azurerm" {
   subscription_id = "f5e76248-4a36-46c4-a53a-399f94f9e210"
 }
 
-resource "azurerm_resource_group" "aks-rg2" {
-  name     = "my-terraform-gr"
+resource "azurerm_resource_group" "muhammadsiddiq-gr" {
+  name     = "muhammadsiddiq-gr"
   location = "Central US"
 
   tags = {
@@ -33,10 +33,10 @@ resource "azurerm_resource_group" "aks-rg2" {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault" "first-key-vault" {
-  name                        = "my-second-vault"
-  location                    = azurerm_resource_group.aks_rg.location
-  resource_group_name         = azurerm_resource_group.aks_rg.name
+resource "azurerm_key_vault" "muhammadsiddiq-vault" {
+  name                        = "muhammadsiddiq-vault"
+  location                    = azurerm_resource_group.muhammadsiddiq-gr.location
+  resource_group_name         = azurerm_resource_group.muhammadsiddiq-gr.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
@@ -65,7 +65,7 @@ resource "azurerm_key_vault" "first-key-vault" {
       "UnwrapKey",
       "Update",
       "Verify",
-      "WrapKey"
+      "WrapKey",
     ]
 
     secret_permissions = [
@@ -76,7 +76,7 @@ resource "azurerm_key_vault" "first-key-vault" {
       "Purge",
       "Recover",
       "Restore",
-      "Set"
+      "Set",
     ]
 
     storage_permissions = [
@@ -93,13 +93,13 @@ resource "azurerm_key_vault" "first-key-vault" {
       "Restore",
       "Set",
       "SetSAS",
-      "Update"
+      "Update",
     ]
   }
 }
 
 resource "azurerm_key_vault_secret" "my-secret" {
-  name         = "sirli-masalliq"
-  value        = "uning-ozi-yoq"
-  key_vault_id = azurerm_key_vault.first-key-vault.id
+  name         = "some-secret"
+  value        = "some-value"
+  key_vault_id = azurerm_key_vault.muhammadsiddiq-vault.id
 }
